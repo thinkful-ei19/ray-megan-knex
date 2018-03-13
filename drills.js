a704f6fb29d657ceb51567f71911203e65ef7f95
leonardom249 <leonardo.megan@gmail.com>
2018-03-13 10:39:53 -0500

    Did part 1 and 2 of morning Knex drills

--- drills.js
+++ drills.js
@@ -1,18 +1,35 @@
 'use strict';
 
 const { DATABASE } = require('./config');
-const knex = require('knex')(DATABASE);
+const knex = require('knex')({
+  client: 'pg',
+  connection: ''
+});
 
 // clear the console before each run
 process.stdout.write('\x1Bc');
 
 // Sample select 
-knex
-  .select()
-  .from('restaurants')
-  .limit(2)
-  .debug(true)
+// knex
+//   .select()
+//   .from('restaurants')
+//   .limit(2)
+//   .debug(true)
+//   .then(results => console.log(results));
+
+// knex
+//   .select()
+//   .table('restaurants')
+//   .then(results => console.log(results));
+
+knex('restaurants')
+  .where('cuisine', 'Italian')
   .then(results => console.log(results));
+
+
+
+
+
 
 
 // Destroy the connection pool
